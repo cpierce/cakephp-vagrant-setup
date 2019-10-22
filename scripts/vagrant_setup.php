2c922f2d3e3a0c445670df37d3afd5955fb8af84
Chris Pierce <cpierce@csdurant.com>
2019-10-21 21:52:12 -0500

    update for vagrant

--- scripts/vagrant_setup.php
+++ scripts/vagrant_setup.php
@@ -67,7 +67,7 @@ $hostname = $options['hostname'];
 $vagrant_file = <<<VAGRANT_FILE_CONTENTS
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
-Vagrant.require_version ">= 2.2.2"
+Vagrant.require_version ">= 2.2.6"
 
 VAGRANT_API_VERSION = "2"
 GUEST_HOSTNAME = "$hostname"
